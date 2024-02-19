66c1c39ea84ec5f63724a766b5d15a9beafb6da3
Austin <austinm9506@gmail.com>
2024-02-19 17:06:03 -0500

    Created base functionality to scan /proc/stat and return CPU values. Credit to mackerelio on GitHub for their osstat library. This helped me understand how to pull values from the /proc/stat file.

---
+++ cpu/cpu.go
@@ -0,0 +1,92 @@
+package cpu
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func GetStats() (*Stats, error) {
+	file, err := os.Open("/proc/stat")
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+	return getCpuStats(file)
+}
+
+type Stats struct {
+	User,
+	Nice,
+	System,
+	Idle,
+	Iowait,
+	Irq,
+	Softirq,
+	Steal,
+	Guest,
+	GuestNice,
+	Total uint64
+	CPUCount,
+	StatCount int
+}
+
+type cpuStat struct {
+	name string
+	ptr  *uint64
+}
+
+func getCpuStats(out io.Reader) (*Stats, error) {
+	scanner := bufio.NewScanner(out)
+	var cpu Stats
+
+	cpuStats := []cpuStat{
+		{"User", &cpu.User},
+		{"Nice", &cpu.Nice},
+		{"System", &cpu.System},
+		{"Idle", &cpu.Idle},
+		{"Iowait", &cpu.Iowait},
+		{"Irq", &cpu.Irq},
+		{"Softirq", &cpu.Softirq},
+		{"Steal", &cpu.Steal},
+		{"Guest", &cpu.Guest},
+		{"GuestNice", &cpu.GuestNice},
+	}
+
+	if !scanner.Scan() {
+		return nil, fmt.Errorf("failed to scan /proc/stat")
+	}
+
+	valStrs := strings.Fields(scanner.Text())[1:]
+	cpu.StatCount = len(valStrs)
+
+	for i, valStr := range valStrs {
+		val, err := strconv.ParseUint(valStr, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan %s from /proc/stat", cpuStats[i].name)
+		}
+		*cpuStats[i].ptr = val
+		cpu.Total += val
+	}
+
+	cpu.Total -= cpu.Guest
+	cpu.Total -= cpu.GuestNice
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "cpu") && unicode.IsDigit(rune(line[3])) {
+			cpu.CPUCount++
+		}
+		if err := scanner.Err(); err != nil {
+			return nil, fmt.Errorf("scan error for /proc/stat: %s", err)
+		}
+	}
+
+	return &cpu, nil
+}
