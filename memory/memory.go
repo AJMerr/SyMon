983b31e93c54d0285e486b507168b37119885aac
Austin <austinm9506@gmail.com>
2024-02-19 21:19:32 -0500

    Created memory.go to read from /proc/meminfo and gather memory usage.

--- memory/memory.go
+++ memory/memory.go
@@ -1,16 +1,17 @@
 package memory
 
-//import (
-// "fmt"
-//"os"
-// "io"
-//"bufio"
-//"strconv"
-//"strings"
-//)
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Stats struct {
-	MemToal,
+	MemTotal,
+	MemUsed,
 	MemFree,
 	MemAvailable,
 	Buffers,
@@ -20,8 +21,73 @@ type Stats struct {
 	SwapCache,
 	SwapFree,
 	SwapTotal,
+	SwapUsed,
 	Slab,
 	PageTotal,
 	Commited_AS,
 	VmAllocUsed uint64
+	Enabed bool
+}
+
+func GetStats() (*Stats, error) {
+	file, err := os.Open("/proc/meminfo")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return getMemStats(file)
+}
+
+func getMemStats(out io.Reader) (*Stats, error) {
+	scanner := bufio.NewScanner(out)
+	var mem Stats
+
+	memStats := map[string]*uint64{
+		"MemTotal":     &mem.MemTotal,
+		"MemFree":      &mem.MemFree,
+		"MemAvailable": &mem.MemAvailable,
+		"Buffers":      &mem.Buffers,
+		"Cached":       &mem.Cached,
+		"Active":       &mem.Active,
+		"Inactive":     &mem.Inactive,
+		"SwapCache":    &mem.SwapCache,
+		"SwapFree":     &mem.SwapFree,
+		"SwapTotal":    &mem.SwapTotal,
+		"Slab":         &mem.Slab,
+		"PageTotal":    &mem.PageTotal,
+		"Commited_AS":  &mem.Commited_AS,
+		"VmAllocUsed":  &mem.VmAllocUsed,
+	}
+
+	for scanner.Scan() {
+		res := scanner.Text()
+		i := strings.IndexRune(res, ':')
+		if i < 0 {
+			continue
+		}
+		index := res[:i]
+		if ptr := memStats[index]; ptr != nil {
+			val := strings.TrimSpace(strings.TrimRight(res[i+1:], "kB"))
+			if v, err := strconv.ParseUint(val, 10, 64); err == nil {
+				*ptr = v * 1024
+			}
+			if index == "MemAvailable" {
+				mem.Enabed = true
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error scanning /proc/meminfo: %s", err)
+	}
+
+	mem.SwapUsed = mem.SwapTotal - mem.SwapFree
+
+	if mem.Enabed {
+		mem.MemUsed = mem.MemTotal - mem.MemAvailable
+	} else {
+		mem.MemUsed = mem.MemTotal - mem.MemFree - mem.Buffers - mem.Cached
+	}
+
+	return &mem, nil
 }
