879c05f12d8b320f9e315e4994b2ee11a630f88d
Austin <austinm9506@gmail.com>
2024-02-22 01:38:04 -0500

    Wrapped the main function in a For loop and added a one second delay for constant updates of stat values.

--- main.go
+++ main.go
@@ -6,38 +6,47 @@ import (
 	"SyMon/memory"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	// Gets cpu statistics from /cpu/cpu.go
-	cpu, err := cpu.GetStats()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+	for {
+		// Gets cpu statistics from /cpu/cpu.go
+		cpu, err := cpu.GetStats()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			return
+		}
+
+		// Gets memory statistics from /memory/memory.go
+		mem, err := memory.GetStats()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+
+		// Converts the bytes to KB or MB respectively for each memory stat
+		convertedMemTotal, unit := memory.ConvertMemVal(mem.MemTotal)
+		convertedMemUsed, unit := memory.ConvertMemVal(mem.MemUsed)
+		convertedMemFree, unit := memory.ConvertMemVal(mem.MemFree)
+
+		// Gets disk statistics from /disk/disk.go
+		diskStats, err := disk.GetStats()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+
+		// Clears the console to refresh output
+		fmt.Print("\033[H\033[2J")
+
+		for _, stat := range diskStats {
+			fmt.Printf("Disk: %s, Reads: %d Writes: %d\n", stat.Name, stat.Reads, stat.Writes)
+		}
+
+		fmt.Printf("memory total: %d %s\n", convertedMemTotal, unit)
+		fmt.Printf("memory used: %d %s\n", convertedMemUsed, unit)
+		fmt.Printf("memory free: %d %s\n", convertedMemFree, unit)
+		fmt.Printf("cpu total: %d\n", cpu.Total)
+
+		time.Sleep(time.Second)
 	}
-
-	// Gets memory statistics from /memory/memory.go
-	mem, err := memory.GetStats()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-	}
-
-	// Converts the bytes to KB or MB respectively for each memory stat
-	convertedMemTotal, unit := memory.ConvertMemVal(mem.MemTotal)
-	convertedMemUsed, unit := memory.ConvertMemVal(mem.MemUsed)
-	convertedMemFree, unit := memory.ConvertMemVal(mem.MemFree)
-
-	// Gets disk statistics from /disk/disk.go
-	diskStats, err := disk.GetStats()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-	}
-
-	for _, stat := range diskStats {
-		fmt.Printf("Disk: %s, Reads: %d Writes: %d\n", stat.Name, stat.Reads, stat.Writes)
-	}
-
-	fmt.Printf("memory total: %d %s\n", convertedMemTotal, unit)
-	fmt.Printf("memory used: %d %s\n", convertedMemUsed, unit)
-	fmt.Printf("memory free: %d %s\n", convertedMemFree, unit)
-	fmt.Printf("cpu total: %d\n", cpu.Total)
 }
