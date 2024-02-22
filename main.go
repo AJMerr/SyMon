c943118bd4be5addba80f7c9a371d3ed5e77671b
Austin <austinm9506@gmail.com>
2024-02-22 00:11:16 -0500

    Tested functionality of the conversion in memory.go and verified it works.

--- main.go
+++ main.go
@@ -13,20 +13,23 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
+
 	mem, err := memory.GetStats()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
+
 	diskStats, err := disk.GetStats()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
+
 	for _, stat := range diskStats {
 		fmt.Printf("Disk: %s, Reads: %d Writes: %d\n", stat.Name, stat.Reads, stat.Writes)
 	}
-	kB := mem.MemTotal / 1024
-	mB := kB / 1024
-	fmt.Printf("memory total: %d bytes\n", mB)
-	fmt.Printf("memory used: %d bytes\n", kB)
+
+	convertedMemTotal, unit := memory.ConvertMemVal(mem.MemTotal)
+	fmt.Printf("memory total: %d %s\n", convertedMemTotal, unit)
+	fmt.Printf("memory used: %d bytes\n", mem.MemUsed)
 	fmt.Printf("cpu total: %d bytes\n", cpu.Total)
 }
