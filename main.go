bd010b1c01d3e1b84b179e15e960923d96e46563
Austin <austinm9506@gmail.com>
2024-02-22 00:28:31 -0500

    Added memory stats for used and free to the main functionality.

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ import (
 )
 
 func main() {
+	// Gets cpu statistics from /cpu/cpu.go
 	cpu, err := cpu.GetStats()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
 
+	// Gets memory statistics from /memory/memory.go
 	mem, err := memory.GetStats()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
 
+	// Converts the bytes to KB or MB respectively for each memory stat
+	convertedMemTotal, unit := memory.ConvertMemVal(mem.MemTotal)
+	convertedMemUsed, unit := memory.ConvertMemVal(mem.MemUsed)
+	convertedMemFree, unit := memory.ConvertMemVal(mem.MemFree)
+
+	// Gets disk statistics from /disk/disk.go
 	diskStats, err := disk.GetStats()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
@@ -28,8 +36,8 @@ func main() {
 		fmt.Printf("Disk: %s, Reads: %d Writes: %d\n", stat.Name, stat.Reads, stat.Writes)
 	}
 
-	convertedMemTotal, unit := memory.ConvertMemVal(mem.MemTotal)
 	fmt.Printf("memory total: %d %s\n", convertedMemTotal, unit)
-	fmt.Printf("memory used: %d bytes\n", mem.MemUsed)
-	fmt.Printf("cpu total: %d bytes\n", cpu.Total)
+	fmt.Printf("memory used: %d %s\n", convertedMemUsed, unit)
+	fmt.Printf("memory free: %d %s\n", convertedMemFree, unit)
+	fmt.Printf("cpu total: %d\n", cpu.Total)
 }
