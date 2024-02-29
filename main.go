f9f3e8b0aeb8aac16c4a306cc46d5d536a670e96
Austin <austinm9506@gmail.com>
2024-02-29 16:57:11 -0500

    Removed other system monitoring tasks for the moment. Added TermUI to main functionality and now displaying CPU percentage in a Gauge.
    TODO: Add Memory and Disk stats back utilizing TermUI.

--- main.go
+++ main.go
@@ -2,61 +2,63 @@ package main
 
 import (
 	"SyMon/cpu"
-	"SyMon/disk"
-	"SyMon/memory"
 	"fmt"
+	"log"
 	"os"
 	"time"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 func main() {
+	// Adding Termui for better visualization
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to init termui: %v", err)
+	}
+	defer ui.Close()
+
 	// Variable that takes in a pointer the the Stats struct from the CPU package
 	var prevCpuStats *cpu.Stats
 
+	g1 := widgets.NewGauge()
+	g1.Title = "CPU Usage:"
+	g1.SetRect(0, 3, 50, 6)
+	g1.BarColor = ui.ColorGreen
+	g1.LabelStyle = ui.NewStyle(ui.ColorWhite)
+	g1.BorderStyle.Fg = ui.ColorWhite
+
+	ui.Render(g1)
+	uiEvents := ui.PollEvents()
+	ticker := time.NewTicker(2 * time.Second).C
+
+MainLoop:
 	for {
-		// Gets cpu statistics from /cpu/cpu.go
-		currentCpuStats, err := cpu.GetStats()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			return
+		select {
+		case <-ticker:
+			// Gets cpu statistics from /cpu/cpu.go
+			currentCpuStats, err := cpu.GetStats()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				return
+			}
+
+			var cpuUsage float64
+			// Calls the CalcCpuPercent function if prevCpuStats are empty and sets prevCpuStats to currentCpuStats for comparison
+			if prevCpuStats != nil {
+				cpuUsage = cpu.CalcCpuPercent(prevCpuStats, currentCpuStats)
+			}
+			prevCpuStats = currentCpuStats
+
+			// Update the CPU gauge
+			g1.Percent = int(cpuUsage)
+			ui.Render(g1)
+
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				break MainLoop
+			}
 		}
-
-		var cpuUsage float64
-		// Calls the CalcCpuPercent function if prevCpuStats are empty and sets prevCpuStats to currentCpuStats fpr comparison
-		if prevCpuStats != nil {
-			cpuUsage = cpu.CalcCpuPercent(prevCpuStats, currentCpuStats)
-		}
-		prevCpuStats = currentCpuStats
-
-		// Gets memory statistics from /memory/memory.go
-		mem, err := memory.GetStats()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-		}
-
-		// Converts the bytes to KB or MB respectively for each memory stat
-		convertedMemTotal, unit := memory.ConvertMemVal(mem.MemTotal)
-		convertedMemUsed, unit := memory.ConvertMemVal(mem.MemUsed)
-		convertedMemFree, unit := memory.ConvertMemVal(mem.MemFree)
-
-		// Gets disk statistics from /disk/disk.go
-		diskStats, err := disk.GetStats()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-		}
-
-		// Clears the console to refresh output
-		fmt.Print("\033[H\033[2J")
-
-		for _, stat := range diskStats {
-			fmt.Printf("Disk: %s, Reads: %d Writes: %d\n", stat.Name, stat.Reads, stat.Writes)
-		}
-
-		fmt.Printf("memory total: %d %s\n", convertedMemTotal, unit)
-		fmt.Printf("memory used: %d %s\n", convertedMemUsed, unit)
-		fmt.Printf("memory free: %d %s\n", convertedMemFree, unit)
-		fmt.Printf("cpu usage: %f%%\n", cpuUsage)
-		// Updated stats will be on a 2 second interval.
-		time.Sleep(2 * time.Second)
 	}
 }
