bd3eaafc38b1b9a4292b249a6e5dca88ee823b83
Austin <austinm9506@gmail.com>
2024-02-29 20:21:12 -0500

    Added Memory Stats back in and formatted output with TermUI.

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"SyMon/cpu"
+	"SyMon/memory"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	ui "github.com/gizak/termui/v3"
@@ -36,7 +38,7 @@ MainLoop:
 	for {
 		select {
 		case <-ticker:
-			// Gets cpu statistics from /cpu/cpu.go
+			// Gets cpu statistics from CPU module
 			currentCpuStats, err := cpu.GetStats()
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s\n", err)
@@ -52,7 +54,48 @@ MainLoop:
 
 			// Update the CPU gauge
 			g1.Percent = int(cpuUsage)
-			ui.Render(g1)
+
+			// Gets memory statistics from Memory module
+			mem, err := memory.GetStats()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				return
+			}
+			memTotal, unit := memory.ConvertMemVal(mem.MemTotal)
+			memUsed, unit := memory.ConvertMemVal(mem.MemUsed)
+			memFree, unit := memory.ConvertMemVal(mem.MemFree)
+
+			memTotalToString := strconv.Itoa(int(memTotal))
+			memUsedToString := strconv.Itoa(int(memUsed))
+			memFreeToString := strconv.Itoa(int(memFree))
+
+			p1 := widgets.NewParagraph()
+			p1.Title = "Total Memory:"
+			p1.BorderStyle.Fg = ui.ColorWhite
+			p1.Text = memTotalToString + unit
+
+			p2 := widgets.NewParagraph()
+			p2.Title = "Memory Used:"
+			p2.BorderStyle.Fg = ui.ColorWhite
+			p2.Text = memUsedToString + unit
+
+			p3 := widgets.NewParagraph()
+			p3.Title = "Memory Free:"
+			p3.BorderStyle.Fg = ui.ColorWhite
+			p3.Text = memFreeToString + unit
+
+			grid := ui.NewGrid()
+			termWidth, termHeight := ui.TerminalDimensions()
+			grid.SetRect(0, 0, termWidth, termHeight)
+
+			grid.Set(
+				ui.NewRow(.5/2, g1),
+				ui.NewRow(.5/2,
+					ui.NewCol(.5/2, p1),
+					ui.NewCol(.5/2, p2),
+					ui.NewCol(.5/2, p3)),
+			)
+			ui.Render(grid)
 
 		case e := <-uiEvents:
 			switch e.ID {
